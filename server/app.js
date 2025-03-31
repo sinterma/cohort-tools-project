fa016fd94d1a4b27a95a977c13e7f7ea7d8d8c8f
Sosha Fedotovskaia <sinterma@gmail.com>
2025-03-31 17:36:01 +0200

    day1 -done

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cohorts =require("./cohorts.json")
+const students=require("./students.json");
+const cors = require("cors");
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -20,7 +24,10 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors({
+  origin: ["http://localhost:5173"]
+}))
+ 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -28,6 +35,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+})
+
+app.get("/api/students", (req,res) => {
+  res.json(students);
+})
 
 
 // START SERVER
